0e6c63a2a0070627c830b70f8582af431daba3f2
Mathieu Amiot <m.amiot@otak-arts.com>
2018-06-06 19:14:43 +0200

    Experimental Req/Rep workflow added

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ extern crate nats;
 extern crate actix;
 
 use actix::prelude::*;
+use std::marker::PhantomData;
 
 pub struct NATSExecutor(nats::Client);
 impl NATSExecutor {
@@ -17,6 +18,14 @@ impl Actor for NATSExecutor {
 pub struct PublishMessage {
     subject: String,
     data: Vec<u8>
+}
+
+impl PublishMessage {
+    pub fn new(subject: String, data: Vec<u8>) -> Self {
+        PublishMessage {
+            subject, data
+        }
+    }
 }
 
 impl Message for PublishMessage {
@@ -36,6 +45,14 @@ pub struct RequestWithReply {
     data: Vec<u8>
 }
 
+impl RequestWithReply {
+    pub fn new(subject: String, data: Vec<u8>) -> Self {
+        RequestWithReply {
+            subject, data
+        }
+    }
+}
+
 impl Message for RequestWithReply {
     type Result = Result<String, nats::NatsError>;
 }
@@ -47,3 +64,38 @@ impl Handler<RequestWithReply> for NATSExecutor {
         self.0.make_request(&msg.subject, &msg.data)
     }
 }
+
+pub struct WaitEventWithInbox<T> where T: From<Vec<u8>> {
+    inbox: String,
+    _meh: PhantomData<T>
+}
+
+impl<T: From<Vec<u8>>> WaitEventWithInbox<T> {
+    pub fn new(inbox: String) -> Self {
+        WaitEventWithInbox {
+            inbox,
+            _meh: PhantomData::default()
+        }
+    }
+}
+
+impl<T: 'static +  From<Vec<u8>>> Message for WaitEventWithInbox<T> {
+    type Result = Result<T, String>;
+}
+
+impl<T: 'static +  From<Vec<u8>>> Handler<WaitEventWithInbox<T>> for NATSExecutor {
+    type Result = Result<T, String>;
+
+    fn handle(&mut self, msg: WaitEventWithInbox<T>, _: &mut Self::Context) -> Self::Result {
+        for e in self.0.events() {
+            match e.inbox {
+                Some(ref inbox) if inbox.clone() == msg.inbox => {
+                    return Ok(e.msg.into());
+                },
+                _ => continue
+            }
+        }
+        Err("No event found".into())
+    }
+}
+
