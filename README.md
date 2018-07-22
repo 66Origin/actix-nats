cbcd444a697e035b111eff2f6ca35c400193acdb
Mathieu Amiot <m.amiot@otak-arts.com>
2018-07-22 19:21:11 +0200

    Prepare for release

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
-# actix-nats
+# Actix-NATS
 
-Actix actor set for NATS Message Broker
+[![Crates.io](https://img.shields.io/crates/v/actix-nats.svg)](https://crates.io/crates/actix-nats)
+[![docs.rs](https://docs.rs/actix-nats/badge.svg)](https://docs.rs/actix-nats)
+
+## Description
+
+Actix actor for NATS Message Broker
+
+## Documentation
+
+Here: [http://docs.rs/actix-nats](http://docs.rs/actix-nats)
+
+## Installation
+
+```toml
+[dependencies]
+actix-nats = "0.3"
+```
+
+## License
+
+Licensed under either of these:
+
+* Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
+   [https://www.apache.org/licenses/LICENSE-2.0](https://www.apache.org/licenses/LICENSE-2.0)
+* MIT license ([LICENSE-MIT](LICENSE-MIT) or
+   [https://opensource.org/licenses/MIT](https://opensource.org/licenses/MIT))
+
+## Yellow Innovation
+
+Yellow Innovation is the innovation laboratory of the French postal service: La Poste.
+
+We create innovative user experiences and journeys through services with a focus on IoT lately.
+
+[Yellow Innovation's website and works](http://yellowinnovation.fr/en/)
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,18 @@
-extern crate nats;
 extern crate actix;
+extern crate nats;
 
 use actix::prelude::*;
 
+/// PublishMessage is a message that publishes a buffer to the NATS queue but doesn't wait for a reply afterwards.
+/// Think of it as the Pub part in PubSub.
 pub struct PublishMessage {
     subject: String,
-    data: Vec<u8>
+    data: Vec<u8>,
 }
 
 impl PublishMessage {
     pub fn new(subject: String, data: Vec<u8>) -> Self {
-        PublishMessage {
-            subject, data
-        }
+        PublishMessage { subject, data }
     }
 }
 
@@ -20,10 +20,12 @@ impl Message for PublishMessage {
     type Result = Result<(), nats::NatsError>;
 }
 
+/// RequestWithReply creates a reply inbox in NATS, publishes a message (Request in terms of NATS grammar)
+/// and waits for the reply to come back.
 pub struct RequestWithReply {
     subject: String,
     data: Vec<u8>,
-    inbox: Option<String>
+    inbox: Option<String>,
 }
 
 impl RequestWithReply {
@@ -31,7 +33,7 @@ impl RequestWithReply {
         RequestWithReply {
             subject,
             data,
-            inbox: None
+            inbox: None,
         }
     }
 }
@@ -40,14 +42,17 @@ impl Message for RequestWithReply {
     type Result = Result<Vec<u8>, nats::NatsError>;
 }
 
+/// Actor to give to Actix to do the background processing of NATS messages/requests
 pub struct NATSExecutorSync(nats::Client);
 impl NATSExecutorSync {
     fn new(client: nats::Client) -> Self {
         NATSExecutorSync(client)
     }
 
+    /// Starts the executor. Give it a number of threads and a factory `Fn() -> nats::Client` that handles client creation and you're good to go.
     pub fn start<F>(threads: usize, client_factory: F) -> Addr<Self>
-        where F: Fn() -> nats::Client + Send + Sync + 'static
+    where
+        F: Fn() -> nats::Client + Send + Sync + 'static,
     {
         SyncArbiter::start(threads, move || Self::new(client_factory()))
     }
@@ -70,6 +75,6 @@ impl Handler<RequestWithReply> for NATSExecutorSync {
 
     fn handle(&mut self, mut msg: RequestWithReply, _: &mut Self::Context) -> Self::Result {
         msg.inbox = Some(self.0.make_request(&msg.subject, &msg.data)?);
-        Ok(self.0.wait()?.msg.into())
+        Ok(self.0.wait()?.msg)
     }
 }
